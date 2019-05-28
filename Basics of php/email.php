8c054f2aee363e5b3674b2ea24af48b4b58e9635
Abhishek38 <abhishekv38@gmail.com>
2019-05-28 13:40:35 +0530

    7th commit

---
+++ Basics of php/email.php
@@ -0,0 +1,18 @@
+<?php 
+$to="abhishek@tezt.com";
+$subject="checking email services of php"
+$message='this <h2>is</h2> the php way of sending email...'
+$header="From:test@gmail.com\n";
+$header.="MIME-Version: 1.0\n";
+$header.="Content-type: text/html ; charset=iso-8859-1\n";
+mail($to,$subject,$message,$<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>);
+
+ ?>
