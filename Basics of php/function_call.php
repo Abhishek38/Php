f5c2f8cc54bc6da52656a23bb1dc5a30bb6ba628
Abhishek38 <abhishekv38@gmail.com>
2019-05-28 13:24:55 +0530

    sixth commit

---
+++ Basics of php/function_call.php
@@ -0,0 +1,7 @@
+<?php 
+include('functions.php');// to use the content of another files first we have to include the file into our working file....(Just like inheritance);
+
+fire();
+scouter(9000);
+
+ ?>
---
+++ Basics of php/functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+function fire(){
+	echo "vegeta tell me the power level".'<br/>';
+}
+
+function scouter($a){
+	echo"its over ".$a;
+}
+
+  ?>
